b887b6ef48aa9e3dfa13691b5057b07824674939
hxj930827 <961372103@qq.com>
2018-11-15 22:36:20 +0800

    idea整合github测试

---
+++ src/main/java/Github.java
@@ -0,0 +1,18 @@
+/**
+ * FileName: Github
+ * Author:   韩旭杰
+ * Date:     2018/11/15 22:35
+ * Description:
+ */
+package main.java;
+
+/**
+ * 说明：〈〉
+ *
+ * @author 韩旭杰
+ * @create 2018/11/15
+ * @since 1.0.0
+ */
+public class Github {
+
+}
